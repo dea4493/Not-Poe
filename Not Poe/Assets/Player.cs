b62558d88168f2f4c5e5b4910083c7eda38f9e90
dea4493 <dea4493@gmail.com>
2017-12-14 01:01:18 -0300

    movement + projectiles

---
+++ Not Poe/Assets/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	Rigidbody2D body; Animator anim;
+	public GameObject aim;
+	float movementSpeed = 100;
+	Vector3 cameraOffset;
+	public GameObject projectile;
+	void Start () {
+		body = GetComponent<Rigidbody2D>();
+		anim = GetComponent<Animator>();
+		cameraOffset = Camera.main.transform.position - transform.position;
+		Cursor.visible = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		aim.transform.position = new Vector3(point.x, point.y, aim.transform.position.z);
+		body.velocity = Vector2.zero;
+		if(Input.GetKey(KeyCode.UpArrow))
+			body.AddForce(Vector2.up*movementSpeed);
+		if(Input.GetKey(KeyCode.DownArrow))
+			body.AddForce(Vector2.down*movementSpeed);
+		if(Input.GetKey(KeyCode.RightArrow))
+			body.AddForce(Vector2.right*movementSpeed);
+		if(Input.GetKey(KeyCode.LeftArrow))
+			body.AddForce(Vector2.left*movementSpeed);
+		Camera.main.transform.position = transform.position + cameraOffset;
+		if(Input.GetMouseButtonDown(0))
+		{
+			GameObject g = Instantiate(projectile, transform.position, Quaternion.identity);
+			Projectile proj = g.GetComponent<Projectile>();
+			proj.speed = .25f;
+			proj.target = aim.transform.position;
+		}
+	}
+}
---
+++ Not Poe/Assets/Projectile.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+	// Use this for initialization
+	public Vector2 target;
+	public float speed;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector2.MoveTowards(transform.position, target, speed);
+		if(transform.position.x == target.x && transform.position.y == target.y) Destroy(gameObject);
+	}
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if(col.tag != "Player") Destroy(gameObject);
+	}
+}
